192edcb3f9fd71af471ff13ad71f5d7b677b582e
unknown0443 <null12901290@gmail.com>
2025-01-28 13:33:20 +0900

    Delete projects/mysite directory

--- projects/mysite/config/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    path('youtube/', include('youtube_api.urls')),  # youtube/ 경로로 접근 가능
-]
--- projects/mysite/youtube_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import YouTubeVideo
-
-@admin.register(YouTubeVideo)
-class YouTubeVideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'video_id', 'published_date')
-    search_fields = ('title', 'description')
